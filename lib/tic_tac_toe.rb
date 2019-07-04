e160f426d330ce7dd6646eb49dce8ad4ea46c75a
Christopher Dent <christopherdent01@gmail.com>
2019-07-04 18:20:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -141,10 +141,10 @@ def play(board)
       turn(board)
    end 
    if won?(board)
-      winner(board) == "X" 
- winner(board) == "O" 
+      
+      
       puts "Congratulations #{winner(board)}!" 
    elsif draw?(board)
-      puts "Cats Game!" 
+      puts "Cat's Game!" 
    end 
 end
