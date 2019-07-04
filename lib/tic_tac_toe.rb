b1134ca94a71a4bd044709f9623836708f938562
Christopher Dent <christopherdent01@gmail.com>
2019-07-04 18:24:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -140,10 +140,9 @@ def play(board)
    until over?(board) 
       turn(board)
    end 
+   
    if won?(board)
-      
-      
-      puts "Congratulations #{winner(board)}!" 
+    puts "Congratulations #{winner(board)}!" 
    elsif draw?(board)
       puts "Cat's Game!" 
    end 
